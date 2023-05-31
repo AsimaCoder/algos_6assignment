7ab842efea0e88968a631969d70499442dcebc9f
AsimaTitanicCoder <asimamyrzabay@example.com>
2023-06-01 05:26:27 +0900

    Vertex update

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,2 +1,12 @@
-public class Vertex {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Vertex<V> {
+    private V data; // Represents the data associated with the vertex
+    private Map<Vertex<V>, Double> adjacentVertices; // Stores the adjacent vertices and their corresponding weights
+
+    public Vertex(V data) {
+        this.data = data; // Initialize the vertex with the provided data
+        this.adjacentVertices = new HashMap<>(); // Create a new HashMap to store the adjacent vertices
+    }
 }
