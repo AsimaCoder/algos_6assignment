e24602b4f98a29b21fbdf9b44936a6a4ba3fad67
AsimaTitanicCoder <asimamyrzabay@example.com>
2023-06-01 05:49:07 +0900

    WeightedGraph update

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -1,2 +1,21 @@
-public class WeightedGraph {
-}
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WeightedGraph<V> {
+    private Map<Vertex<V>, List<Vertex<V>>> adjacencyList;
+
+    public WeightedGraph() {
+        adjacencyList = new HashMap<>();
+    }
+
+    public void addVertex(Vertex<V> vertex) {
+        adjacencyList.put(vertex, new ArrayList<>());
+    }
+
+    public void addEdge(Vertex<V> source, Vertex<V> destination, double weight) {
+        validateVertex(source);
+        validateVertex(destination);
+        adjacencyList.get(source).add(destination);
+        source.addAdjacentVertex(destination, weight);
+    }}
