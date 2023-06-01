47c52a89d42c9c3eadcf68ecb424887d7ff2148f
AsimaTitanicCoder <asimamyrzabay@example.com>
2023-06-01 09:26:36 +0900

    BreadthFirstSearch update

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -1,2 +1,8 @@
-public class BreadthFirstSearch {
-}
+import java.util.*;
+
+public class BreadthFirstSearch<V> implements Search<V> {
+    private final WeightedGraph<V> graph;
+
+    public BreadthFirstSearch(WeightedGraph<V> graph) {
+        this.graph = graph;
+    }}
