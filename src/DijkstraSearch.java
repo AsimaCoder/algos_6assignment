467778b77af97a1909de3a2183fc44cae7b47094
AsimaTitanicCoder <asimamyrzabay@example.com>
2023-06-01 09:47:09 +0900

    DijkstraSearch update

--- src/DijkstraSearch.java
+++ src/DijkstraSearch.java
@@ -1,2 +1,11 @@
-public class DijkstraSearch {
+import java.util.*;
+
+public class DijkstraSearch<V> implements Search<V> {
+    private final WeightedGraph<V> graph;
+
+    public DijkstraSearch(WeightedGraph<V> graph) {
+        this.graph = graph;
+    }
+
 }
+
