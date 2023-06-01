25864ef7c6dea70c5da5c267532de94f041c13f7
AsimaTitanicCoder <asimamyrzabay@example.com>
2023-06-01 10:07:42 +0900

    Comments(2)

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -4,7 +4,8 @@ public class BreadthFirstSearch<V> implements Search<V> {
     private final WeightedGraph<V> graph;
 
     public BreadthFirstSearch(WeightedGraph<V> graph) {
-        this.graph = graph;
+        this.graph = graph;         // Constructor to initialize the BreadthFirstSearch object with the provided graph
+
     }
 
     public List<Vertex<V>> traverse(Vertex<V> start) {
@@ -16,12 +17,14 @@ public class BreadthFirstSearch<V> implements Search<V> {
         visited.add(start);
 
         while (!queue.isEmpty()) {
-            Vertex<V> currentVertex = queue.poll();
-            traversalOrder.add(currentVertex);
+            Vertex<V> currentVertex = queue.poll(); // Retrieve and remove the first vertex from the queue
+            traversalOrder.add(currentVertex); // Add the current vertex to the traversal order
 
             List<Vertex<V>> neighbors = graph.getAdjacencyList().get(currentVertex);
-            for (Vertex<V> neighbor : neighbors) {
-                if (!visited.contains(neighbor)) {
+            for (Vertex<V> neighbor : neighbors) {                 // Iterate through each neighbor of the current vertex
+
+                if (!visited.contains(neighbor)) {                    // If the neighbor has not been visited, enqueue it and mark it as visited
+
                     queue.offer(neighbor);
                     visited.add(neighbor);
                 }
