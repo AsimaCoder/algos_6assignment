18033e219998a3e550aea6a945af17012067f49d
AsimaTitanicCoder <asimamyrzabay@example.com>
2023-06-01 09:32:45 +0900

    BreadthFirstSearch update new method added

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -5,4 +5,29 @@ public class BreadthFirstSearch<V> implements Search<V> {
 
     public BreadthFirstSearch(WeightedGraph<V> graph) {
         this.graph = graph;
-    }}
+    }
+
+    public List<Vertex<V>> traverse(Vertex<V> start) {
+        List<Vertex<V>> traversalOrder = new ArrayList<>();
+        Set<Vertex<V>> visited = new HashSet<>();
+        Queue<Vertex<V>> queue = new LinkedList<>();
+
+        queue.offer(start);
+        visited.add(start);
+
+        while (!queue.isEmpty()) {
+            Vertex<V> currentVertex = queue.poll();
+            traversalOrder.add(currentVertex);
+
+            List<Vertex<V>> neighbors = graph.getAdjacencyList().get(currentVertex);
+            for (Vertex<V> neighbor : neighbors) {
+                if (!visited.contains(neighbor)) {
+                    queue.offer(neighbor);
+                    visited.add(neighbor);
+                }
+            }
+        }
+
+        return traversalOrder;
+    }
+}
